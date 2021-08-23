a8e80f7152f09dcb103a12bf3312310066b10928
Huan353 <dydo00@bk.ru>
2021-08-23 19:47:29 +0300

    footer

--- project/js/main.js
+++ project/js/main.js
@@ -1,13 +1,37 @@
 $(document).ready(function () {
   let currentFloor = 2;
+  let modal = $(".modal");
   let counterUp = $(".counter-up");
   let floorPath = $(".home-image path");
   let counterDown = $(".counter-down");
+  let closeButton = $(".modal-close-button");
+  let buttonPrimary = $(".button-pimary");
+
+  //функци наведения на этаж
   floorPath.on("mouseover", function () {
     floorPath.removeClass("current-floor");
     currentFloor = $(this).attr("data-flor");
     $(".counter").text(currentFloor);
   });
+
+  //функция раскрытия модального окна при нажатии на этиж
+  floorPath.on("click", function () {
+    modal.toggleClass("is-open");
+    $(".modal-counter").text(currentFloor);
+  });
+
+  //функция закрытия модального окна при нажатии на крестик
+  closeButton.on("click", function () {
+    modal.removeClass("is-open");
+  });
+
+  //функция открытия модального окна при нажатии на кнопку выбора этажа
+  buttonPrimary.on("click", function () {
+    modal.toggleClass("is-open");
+    $(".modal-counter").text(currentFloor);
+  });
+
+  //функци клика на кнопку вверх
   counterUp.on("click", function () {
     if (currentFloor < 18) {
       currentFloor++;
@@ -20,6 +44,8 @@ $(document).ready(function () {
       $(`[data-flor = ${usCurrentFloor}]`).toggleClass("current-floor");
     }
   });
+
+  //функци клика на кнопку вниз
   counterDown.on("click", function () {
     if (currentFloor > 2) {
       currentFloor--;
